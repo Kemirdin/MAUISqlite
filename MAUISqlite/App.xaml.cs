fee024648ef72573fb0945fd098ecf40fd044682
K3M1RD1N <33654086+Kemirdin@users.noreply.github.com>
2023-07-29 22:29:49 +0200

    Update App.xaml.cs

--- MAUISqlite/App.xaml.cs
+++ MAUISqlite/App.xaml.cs
@@ -1,5 +1,8 @@
 ﻿namespace MAUISqlite;
 
+// This is the entry point of the application. The App class is a partial class
+// that represents the application and is responsible for initializing it.
+
 public partial class App : Application
 {
 	public App()
