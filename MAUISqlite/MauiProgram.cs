d8cb87f1c63cec2b0db12353d0af2daa6d8d9ccd
K3M1RD1N <33654086+Kemirdin@users.noreply.github.com>
2023-07-31 22:17:40 +0200

    Update MauiProgram.cs

--- MAUISqlite/MauiProgram.cs
+++ MAUISqlite/MauiProgram.cs
@@ -2,20 +2,29 @@
 using Microsoft.Extensions.Logging;
 
 // The namespace for the MAUISqlite application.
-namespace MAUISqlite;
-
-public static class MauiProgram
+namespace MAUISqlite
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+    // A static class for the Maui program.
+    public static class MauiProgram
+    {
+        // The main method that creates and configures the Maui app.
+        public static MauiApp CreateMauiApp()
+        {
+            // Create a new Maui app builder instance.
+            var builder = MauiApp.CreateBuilder();
+
+            // Use the App class as the startup entry point for the app.
+            builder.UseMauiApp<App>()
+
+            // Configure custom fonts for the app.
+            .ConfigureFonts(fonts =>
+            {
+                // Add the "OpenSans-Regular.ttf" font with the alias "OpenSansRegular".
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                
+                // Add the "OpenSans-Semibold.ttf" font with the alias "OpenSansSemibold".
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
 
 #if DEBUG
 		builder.Logging.AddDebug();
