b06d027c6e44f6b6cea4fa46effa687a9fbcdfc2
K3M1RD1N <33654086+Kemirdin@users.noreply.github.com>
2023-08-02 22:48:17 +0200

    Update MainPage.xaml.cs

--- MAUISqlite/MainPage.xaml.cs
+++ MAUISqlite/MainPage.xaml.cs
@@ -12,16 +12,21 @@ public partial class MainPage : ContentPage
 
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+   // This method is triggered when the "CounterBtn" button is clicked
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        // Increment the counter each time the button is clicked
+        count++;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+        // Update the text of the "CounterBtn" to display the number of times it has been clicked
+        // Use a conditional statement to adjust the text based on the count value
+        if (count == 1)
+            CounterBtn.Text = $"Clicked {count} time";
+        else
+            CounterBtn.Text = $"Clicked {count} times";
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+        // Announce the updated text for accessibility purposes using SemanticScreenReader
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
 }
 
